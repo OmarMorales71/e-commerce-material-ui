f5000259d7f7f210e0d24ed8027c8ef5d1b5a2da
mancubus <mancubus@sourcedevil.com>
2020-08-06 18:37:16 -0500

    Rebuilding index

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import {ThemeProvider, makeStyles} from '@material-ui/core/styles'
-import theme from './themeConfig'
-import PrimarySearchNavBar from './components/PrimarySearchNavBar';
-import PrimaryHeader from './components/PrimaryHeader'
-import FeaturedProducts from './components/FeaturedProducts';
-
-const useStyles=makeStyles(theme=>({
-  comp:{
-    marginBottom: 500
-  }
-}))
+import React from "react";
+import PrimarySearchNavBar from "./components/PrimarySearchNavBar";
+import PrimaryHeader from "./components/PrimaryHeader";
+import FeaturedProducts from "./components/FeaturedProducts";
+import { Grid } from "@material-ui/core";
 
 function App() {
-  const classes= useStyles()
   return (
-    <ThemeProvider theme={theme}>
-      <PrimarySearchNavBar disableElevation/>
-      <PrimaryHeader gutterBottom/>
-      <br /><br />
-      <FeaturedProducts className={classes.comp}/>
-    </ThemeProvider>
+
+    <>
+        <PrimarySearchNavBar />
+      
+          <PrimaryHeader />
+<br />
+<br />
+          <FeaturedProducts />
+      </>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import {ThemeProvider} from '@material-ui/core'
+import theme from './themeConfig';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
