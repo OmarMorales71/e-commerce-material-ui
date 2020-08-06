ea977e852fd0fbd688cffc2ab4fa51a3adb3261e
mancubus <mancubus@sourcedevil.com>
2020-08-06 18:38:52 -0500

    Featured Products Implemented

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,53 +1,37 @@
-import React from 'react'
-import { Typography } from "@material-ui/core";
-import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import CardMedia from "@material-ui/core/CardMedia";
-import Card from "@material-ui/core/Card";
-
-const useStyle=makeStyles(theme=>({
-    mediaCard:{
-        height: 0,
-        paddingTop: "56.25%"
-    }
-}))
+import React from "react";
+import { Grid, Typography, TextareaAutosize } from "@material-ui/core";
+import FeaturedCard from "./FeaturedCard";
 
 
-const FeaturedProducts = () =>{
-    const classes = useStyle()
-    return(
-        <>
-        <Typography variant="h4" color="initial" align="center" gutterBottom>
+const FeaturedProducts = () => {
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12} >
+          <Typography variant="h3" color="initial" align="center" gutterBottom>
             Featured Products
-        </Typography>
-        <Container maxWidth="md" component="main">
-        <Grid container spacing={2} alignItems="flex-end">
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardMedia
-                style={{ height: 0, paddingTop: "70%" }}
-                image="https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2018/01/imagen-aleatoria-destacada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
-                title="lorem ipsum"
-              />
-            </Card>
-            
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardMedia
-                style={{ height: 0, paddingTop: "70%" }}
-                image="https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2018/01/imagen-aleatoria-destacada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
-                title="lorem ipsum"
-              />
-            </Card>
-            
-          </Grid>
-          
+          </Typography>
         </Grid>
-      </Container>
-        </>
-    )
-}
+        <Grid item container spacing={5} alignItems="center"  justify='center'>
+          
+          <Grid item xs={10} sm={5}>
+            <FeaturedCard  
+            title="PRODUCT NAME" 
+            price="$300"
+            imageUrl="https://icdn2.digitaltrends.com/image/digitaltrends_es/bonavita-connoiseur-768x768.jpg"
+            />
+          </Grid>
+          <Grid item xs={10} sm={5}>
+          <FeaturedCard 
+            title="PRODUCT NAME" 
+            price="$300"
+            imageUrl="https://icdn2.digitaltrends.com/image/digitaltrends_es/bonavita-connoiseur-768x768.jpg"
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-export default FeaturedProducts
+export default FeaturedProducts;
