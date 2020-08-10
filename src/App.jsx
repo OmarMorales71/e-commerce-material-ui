983df59337c58ca118d295403ddce33c06e6983b
mancubus <mancubus@sourcedevil.com>
2020-08-10 13:54:50 -0500

    added cart page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom"
 import ShopPage from "./components/Pages/ShopPage";
 import HomePage from "./components/Pages/HomePage";
 import ProductPage from './components/Pages/ProductPage'
+import CartPage from './components/Pages/CartPage'
 const App = ()=>{
 
   return ( 
@@ -11,6 +12,7 @@ const App = ()=>{
       <Route exact path="/" render={(props) => <HomePage {...props}/>} />
       <Route exact path="/shop" render={(props) => <ShopPage {...props}/>} />
       <Route exact path="/product" render={(props) => <ProductPage {...props}/>} />
+      <Route exact path="/cart" render={(props) => <CartPage {...props}/>} />
       </Switch>
   );
 }
---
+++ src/components/Pages/CartPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../NavBar'
+const CartPage=({history})=>{
+    return (
+        <>
+        <NavBar history={history} />
+        
+        </>
+    )
+}
+
+export default CartPage
